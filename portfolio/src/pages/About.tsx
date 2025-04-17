f38a5f71b087512326d1a31b84cb9d1ebf487178
O-Vanessa-R <vanessa.rolland@oclock.school>
2025-04-17 20:06:27 +0200

    feat: add about page

---
+++ portfolio/src/pages/About.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <main className="main">
+            <h1 className="h1">A propos de moi</h1>
+        </main>
+    );
+};
+
+export default AboutPage;
